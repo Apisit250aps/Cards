77986715b4459b31e01e212621ea291f3a364e34
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2023-11-25 10:24:12 +0700

    flicking

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import Flicking from "@egjs/vue3-flicking/dist/flicking.esm";
+// import Flicking from "@egjs/vue3-flicking/dist/flicking.esm";
 import "@egjs/vue3-flicking/dist/flicking-inline.css";
 
 createApp(App).mount('#app');
