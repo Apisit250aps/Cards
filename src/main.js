531876626fe9a7a8b9f5f565b964e679a2584185
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2023-11-26 10:10:51 +0700

    redeploy

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 
-import axios from 'axios';
+// import axios from 'axios';
 
-// import Flicking from "@egjs/vue3-flicking/dist/flicking.esm";
+import Flicking from "@egjs/vue3-flicking/dist/flicking.esm";
 import "@egjs/vue3-flicking/dist/flicking-inline.css";
 
-createApp(App).mount('#app');
+createApp(App).use(Flicking).mount('#app');
 
